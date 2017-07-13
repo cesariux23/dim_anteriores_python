f6d5bdccb9c36296b50cb9e58348327089a77e70
Cèsar Encarnación <cesariux23@gmail.com>
2017-07-12 23:26:36 -0500

    :wip:

--- estructura.py
+++ estructura.py
@@ -1,12 +1,21 @@
 import os
-
+import glob
+from openpyxl import load_workbook, worksheet, utils
 meses = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC']
 quincenas = ['1RA', '2DA']
 directorios = []
+ruta = os.getcwd()
+a = 2014
+for mes in meses:
 
-a=2014
-for m in meses:
-    for q in quincenas:
-        carpeta = '{0}/{1}'.format(m,q)
-        inicial = '{0} {1}'.format(q,m)
-        os.makedirs('{0}/{1}'.format(a,carpeta))
+    for quincena in quincenas:
+        carpeta = '{0}/{1}'.format(mes, quincena)
+        #inicial = '{0}/{1}'.format(q,m)
+        #os.makedirs('{0}/{1}'.format(a,carpeta))
+        files = glob.glob(('{0}/{1}/{2}/*.xlsx'.format(ruta, a, carpeta)))
+        leer_libro(files, None)
+    
+def leer_libro (archivos, libro):
+    for file in files:
+        print(file)
+
